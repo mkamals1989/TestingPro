4905fc0d9883dda0f4d1956864ee65de2ad4d67f
Kamal Selvaraj <mkamals1989@gmail.com>
2017-11-09 00:12:00 +0530

    Second Commit From laptop

---
+++ app/src/main/java/com/example/jeesan_2/testingpro/Concepts/WriteFile.java
@@ -0,0 +1,20 @@
+package com.example.jeesan_2.testingpro.Concepts;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.jeesan_2.testingpro.R;
+
+/**
+ * Created by KAMAL on 11/8/2017.
+ */
+
+public class WriteFile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.writefile);
+    }
+}
