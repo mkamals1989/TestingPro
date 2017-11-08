9690559a983047dc5eaca452719623b5272126f4
Kamal Selvaraj <mkamals1989@gmail.com>
2017-11-09 01:21:17 +0530

    Read/Write File commit

---
+++ app/src/main/java/com/example/jeesan_2/testingpro/Concepts/ReadFile.java
@@ -0,0 +1,55 @@
+package com.example.jeesan_2.testingpro.Concepts;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.jeesan_2.testingpro.R;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by KAMAL on 11/9/2017.
+ */
+
+public class ReadFile extends AppCompatActivity {
+
+    @BindView(R.id.tv)
+    TextView tv;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.readfile);
+        ButterKnife.bind(this);
+    }
+
+    public void loadMethod(View v) {
+        FileInputStream fileInputStream = null;
+        StringBuffer buff=new StringBuffer();
+        try {
+            fileInputStream = openFileInput("SampleFile.txt");
+            int read = -1;
+            while ((read = fileInputStream.read()) != -1) {
+                buff.append((char)read);
+            }
+            tv.setText(buff.toString());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                fileInputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/jeesan_2/testingpro/Concepts/WriteFile.java
+++ app/src/main/java/com/example/jeesan_2/testingpro/Concepts/WriteFile.java
@@ -1,10 +1,22 @@
 package com.example.jeesan_2.testingpro.Concepts;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.jeesan_2.testingpro.R;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by KAMAL on 11/8/2017.
@@ -12,9 +24,47 @@ import com.example.jeesan_2.testingpro.R;
 
 public class WriteFile extends AppCompatActivity {
 
+    @BindView(R.id.edt)
+    EditText edt;
+
+    @BindView(R.id.save)
+    Button save;
+
+    @BindView(R.id.next)
+    Button next;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.writefile);
+        ButterKnife.bind(this);
+    }
+
+    public void saveMethod(View v) {
+        String text = edt.getText().toString();
+        if (text != null && !text.isEmpty()) {
+            FileOutputStream fileOutputStream = null;
+            try {
+                fileOutputStream = openFileOutput("SampleFile.txt", MODE_PRIVATE);
+                fileOutputStream.write(text.getBytes());
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }finally {
+                try {
+                    fileOutputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            Toast.makeText(this, "File saved successfully.", Toast.LENGTH_SHORT).show();
+        } else
+            Toast.makeText(this, "Fill text", Toast.LENGTH_SHORT).show();
+    }
+
+    public void nextMethod(View v) {
+        Intent intent = new Intent(this,ReadFile.class);
+        startActivity(intent);
     }
 }
