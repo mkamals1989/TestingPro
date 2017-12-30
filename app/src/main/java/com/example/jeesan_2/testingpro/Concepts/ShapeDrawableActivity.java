5167acf6b574b4891d25f33d87ae8789a9087e13
Kamal <mkamals1989@gmail.com>
2017-12-30 15:46:01 +0530

    Shape

---
+++ app/src/main/java/com/example/jeesan_2/testingpro/Concepts/ShapeDrawableActivity.java
@@ -0,0 +1,40 @@
+package com.example.jeesan_2.testingpro.Concepts;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.jeesan_2.testingpro.CustomUtility.PermissionCheck;
+import com.example.jeesan_2.testingpro.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * Created by "Kamal S" on 04/11/2017.
+ */
+
+public class ShapeDrawableActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv)
+    TextView tv;
+
+    @BindView(R.id.img)
+    ImageView img;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn)
+    public void submit() {
+
+    }
+}
