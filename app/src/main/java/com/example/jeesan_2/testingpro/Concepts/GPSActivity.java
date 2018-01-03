f4b5035e7f34bb857d475604344e6a7882806d99
Kamal Selvaraj <mkamals1989@gmail.com>
2018-01-03 22:44:45 +0530

    Gps Activity update

---
+++ app/src/main/java/com/example/jeesan_2/testingpro/Concepts/GPSActivity.java
@@ -0,0 +1,409 @@
+package com.example.jeesan_2.testingpro.Concepts;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.jeesan_2.testingpro.CustomUtility.PermissionHelper;
+import com.example.jeesan_2.testingpro.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * Created by http://askfortricks.com
+ * <p>
+ * step1 :buildGoogleApiClient
+ * step2: createLocationRequest
+ * step3: buildLocationSettingsRequest
+ * step 4:on click of button detect location check the location is on/off using checkLocationSettings method.
+ * step 5:on click of the options in location dialog
+ * step 6 :depending on the action taken on dialog (startResolution for result and check the action in onActivityResult
+ */
+
+public class GPSActivity extends AppCompatActivity implements ResultCallback<LocationSettingsResult>, LocationListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    protected static final int REQUEST_CHECK_SETTINGS = 0x1;
+    Button button;
+    TextView textView;
+    protected static final String TAG = "GPSActivity";
+    PermissionHelper mPermissionHelper;
+    protected GoogleApiClient mGoogleApiClient;
+    protected LocationRequest mLocationRequest;
+    protected LocationSettingsRequest mLocationSettingsRequest;
+    protected Location mCurrentLocation;
+    protected Boolean mRequestingLocationUpdates;
+    protected String mLastUpdateTime;
+    int RQS_GooglePlayServices = 0;
+    StringBuilder sb = new StringBuilder();
+    /**
+     * The desired interval for location updates. Inexact. Updates may be more or less frequent.
+     */
+    public static final long UPDATE_INTERVAL_IN_MILLISECONDS = 10000;
+
+    /**
+     * The fastest rate for active location updates. Exact. Updates will never be more frequent
+     * than this value.
+     */
+    public static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS =
+            UPDATE_INTERVAL_IN_MILLISECONDS / 2;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        GoogleApiAvailability googleAPI = GoogleApiAvailability.getInstance();
+        int resultCode = googleAPI.isGooglePlayServicesAvailable(this);
+        if (resultCode == ConnectionResult.SUCCESS) {
+            mGoogleApiClient.connect();
+        } else {
+            googleAPI.getErrorDialog(this, resultCode, RQS_GooglePlayServices);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Within {@code onPause()}, we pause location updates, but leave the
+        // connection to GoogleApiClient intact.  Here, we resume receiving
+        // location updates if the user has requested them.
+        if (mGoogleApiClient.isConnected() && mRequestingLocationUpdates) {
+            //  Toast.makeText(FusedLocationWithSettingsDialog.this, "location was already on so detecting location now", Toast.LENGTH_SHORT).show();
+            startLocationUpdates();
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Stop location updates to save battery, but don't disconnect the GoogleApiClient object.
+        if (mGoogleApiClient.isConnected()) {
+            stopLocationUpdates();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gps);
+
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+        mPermissionHelper = new PermissionHelper(this);
+
+        mRequestingLocationUpdates = false;
+        mLastUpdateTime = "";
+
+        // Kick off the process of building the GoogleApiClient, LocationRequest, and
+        // LocationSettingsRequest objects.
+
+        //step 1
+        buildGoogleApiClient();
+
+        //step 2
+        createLocationRequest();
+
+        //step 3
+        buildLocationSettingsRequest();
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(GPSActivity.this, "Hi", Toast.LENGTH_SHORT).show();
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (mPermissionHelper.checkLocationPermission()) {
+                        Toast.makeText(GPSActivity.this, "Permission Already Granted", Toast.LENGTH_SHORT).show();
+                        checkLocationSettings();
+                    } else
+                        mPermissionHelper.checkLocationPermission();
+                } else {
+                    Toast.makeText(GPSActivity.this, "Run time Permission not needed here.", Toast.LENGTH_SHORT).show();
+                    checkLocationSettings();
+                }
+
+
+            }
+        });
+    }
+
+    //step 1
+    protected synchronized void buildGoogleApiClient() {
+        Log.i(TAG, "Building GoogleApiClient");
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    //step 2
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+
+        // Sets the desired interval for active location updates. This interval is
+        // inexact. You may not receive updates at all if no location sources are available, or
+        // you may receive them slower than requested. You may also receive updates faster than
+        // requested if other applications are requesting location at a faster interval.
+        mLocationRequest.setInterval(UPDATE_INTERVAL_IN_MILLISECONDS);
+
+        // Sets the fastest rate for active location updates. This interval is exact, and your
+        // application will never receive updates faster than this value.
+        mLocationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS);
+
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+
+    //step 3
+    protected void buildLocationSettingsRequest() {
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(mLocationRequest);
+        mLocationSettingsRequest = builder.build();
+    }
+
+
+    //step 4
+
+    protected void checkLocationSettings() {
+        PendingResult<LocationSettingsResult> result =
+                LocationServices.SettingsApi.checkLocationSettings(
+                        mGoogleApiClient,
+                        mLocationSettingsRequest
+                );
+        result.setResultCallback(this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PermissionHelper.TAG_LOCATION_RESULTCODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission Granted by user", Toast.LENGTH_SHORT).show();
+                checkLocationSettings();
+            } else if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                Toast.makeText(this, "Permission not Granted by user", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    /**
+     * Invoked when settings dialog is opened and action taken
+     *
+     * @param locationSettingsResult This below OnResult will be used by settings dialog actions.
+     */
+
+    //step 5
+    @Override
+    public void onResult(LocationSettingsResult locationSettingsResult) {
+
+        final Status status = locationSettingsResult.getStatus();
+        switch (status.getStatusCode()) {
+            case LocationSettingsStatusCodes.SUCCESS:
+                Log.i(TAG, "All location settings are satisfied.");
+
+                Toast.makeText(GPSActivity.this, "Location is already on.", Toast.LENGTH_SHORT).show();
+                startLocationUpdates();
+                break;
+            case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                Log.i(TAG, "Location settings are not satisfied. Show the user a dialog to" +
+                        "upgrade location settings ");
+
+                try {
+                    // Show the dialog by calling startResolutionForResult(), and check the result
+                    // in onActivityResult().
+                    Toast.makeText(GPSActivity.this, "Location dialog will be open", Toast.LENGTH_SHORT).show();
+                    //
+
+                    //move to step 6 in onActivityResult to check what action user has taken on settings dialog
+                    status.startResolutionForResult(GPSActivity.this, REQUEST_CHECK_SETTINGS);
+                } catch (IntentSender.SendIntentException e) {
+                    Log.i(TAG, "PendingIntent unable to execute request.");
+                }
+                break;
+            case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                Log.i(TAG, "Location settings are inadequate, and cannot be fixed here. Dialog " +
+                        "not created.");
+                break;
+        }
+    }
+
+
+    /**
+     * This OnActivityResult will listen when
+     * case LocationSettingsStatusCodes.RESOLUTION_REQUIRED: is called on the above OnResult
+     */
+    //step 6:
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            // Check for the integer request code originally supplied to startResolutionForResult().
+            case REQUEST_CHECK_SETTINGS:
+                switch (resultCode) {
+                    case Activity.RESULT_OK:
+                        Log.i(TAG, "User agreed to make required location settings changes.");
+                        startLocationUpdates();
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Log.i(TAG, "User chose not to make required location settings changes.");
+                        break;
+                }
+                break;
+        }
+    }
+
+    /**
+     * Requests location updates from the FusedLocationApi.
+     */
+    protected void startLocationUpdates() {
+
+
+        goAndDetectLocation();
+
+    }
+
+    public void goAndDetectLocation() {
+        if (mPermissionHelper.checkLocationPermission())
+            LocationServices.FusedLocationApi.requestLocationUpdates(
+                    mGoogleApiClient,
+                    mLocationRequest,
+                    this
+            ).setResultCallback(new ResultCallback<Status>() {
+                @Override
+                public void onResult(Status status) {
+                    mRequestingLocationUpdates = true;
+                    //     setButtonsEnabledState();
+                }
+            });
+    }
+
+    /**
+     * Removes location updates from the FusedLocationApi.
+     */
+    protected void stopLocationUpdates() {
+        // It is a good practice to remove location requests when the activity is in a paused or
+        // stopped state. Doing so helps battery performance and is especially
+        // recommended in applications that request frequent location updates.
+        LocationServices.FusedLocationApi.removeLocationUpdates(
+                mGoogleApiClient,
+                this
+        ).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(Status status) {
+                mRequestingLocationUpdates = false;
+                //   setButtonsEnabledState();
+            }
+        });
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.w(TAG, "onConnected");
+
+        if (mCurrentLocation == null) {
+            if (mPermissionHelper.checkLocationPermission())
+                mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+            updateLocationUI();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mCurrentLocation = location;
+        mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+        updateLocationUI();
+        Toast.makeText(this, "Location Updated",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * Sets the value of the UI fields for the location latitude, longitude and last update time.
+     */
+    private void updateLocationUI() {
+        if (mCurrentLocation != null) {
+            if (sb != null) {
+                sb.append("Latitude : " + mCurrentLocation.getLatitude() + "\n" +
+                        "Longitude : " + mCurrentLocation.getLongitude() + "\n" +
+                        "Time : " + mLastUpdateTime + "\n");
+            }
+//            textView.setText(sb.toString());
+
+            updateCityAndPincode(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
+        }
+    }
+
+
+    /**
+     * This updateCityAndPincode method uses Geocoder api to map the latitude and longitude into city location or pincode.
+     * We can retrieve many details using this Geocoder class.
+     * <p>
+     * And yes the Geocoder will not work unless you have data connection or wifi connected to internet.
+     */
+
+
+    private void updateCityAndPincode(double latitude, double longitude) {
+        try {
+            Geocoder gcd = new Geocoder(GPSActivity.this, Locale.getDefault());
+            List<Address> addresses = gcd.getFromLocation(latitude, longitude, 1);
+            if (addresses.size() > 0) {
+                if (sb != null) {
+                    sb.append("City=" + addresses.get(0).getLocality() + "\n" +
+                            "Pincode=" + addresses.get(0).getPostalCode());
+                    textView.setText(sb.toString());
+                }
+                //  System.out.println(addresses.get(0).getLocality());
+            }
+
+        } catch (Exception e) {
+            Log.e(TAG, "exception:" + e.toString());
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/jeesan_2/testingpro/CustomUtility/PermissionHelper.java
@@ -0,0 +1,228 @@
+package com.example.jeesan_2.testingpro.CustomUtility;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+
+public class PermissionHelper {
+
+    public final static int TAG_LOCATION_RESULTCODE = 101;
+    public final static int TAG_STORAGE_READ_WRITE_RESULTCODE = 102;
+    public final static int TAG_CAMERA_RESULTCODE = 103;
+    public final static int TAG_RECOERD_AUDIO_RESULTCODE = 104;
+    public final static int TAG_SMS_RESULTCODE = 105;
+    public final static int TAG_CONTACTS_RESULTCODE = 106;
+    public final static int TAG_SENSORS_RESULTCODE = 107;
+    public final static int TAG_CALLPHONE_RESULTCODE = 108;
+
+    @SuppressLint("InlinedApi")
+    private static String[] PERMISSIONS_STORAGE = {Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+    private static String[] PERMISSIONS_LOCATION = {Manifest.permission.ACCESS_FINE_LOCATION};
+
+    private static String[] PERMISSIONS_CAMERA = {Manifest.permission.CAMERA};
+
+    private static String[] PERMISSIONS_RECORD_AUDIO = {Manifest.permission.RECORD_AUDIO};
+
+    private static String[] PERMISSIONS_SMS = {Manifest.permission.READ_SMS,
+            Manifest.permission.SEND_SMS, Manifest.permission.RECEIVE_SMS};
+
+    private static String[] PERMISSIONS_CONTACTS = {Manifest.permission.READ_CONTACTS,
+            Manifest.permission.WRITE_CONTACTS, Manifest.permission.GET_ACCOUNTS};
+
+    private static String[] PERMISSIONS_SENSORS = {Manifest.permission.BODY_SENSORS};
+
+    private static String[] PERMISSIONS_CALL = {Manifest.permission.CALL_PHONE};
+
+    private Activity mActivity = null;
+
+    public PermissionHelper(Activity mActivity) {
+        this.mActivity = (Activity) mActivity;
+    }
+
+    public PermissionHelper(AppCompatActivity mActivity) {
+        this.mActivity = (Activity) mActivity;
+    }
+
+    public PermissionHelper(FragmentActivity mActivity) {
+        this.mActivity = (Activity) mActivity;
+    }
+
+    // Check Location Permission
+    public boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(mActivity,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                ) {
+
+            if (ActivityCompat
+                    .shouldShowRequestPermissionRationale(mActivity,
+                            Manifest.permission.ACCESS_FINE_LOCATION)) {
+                ActivityCompat.requestPermissions(mActivity, PERMISSIONS_LOCATION,
+                        TAG_LOCATION_RESULTCODE);
+            } else {
+                ActivityCompat.requestPermissions(mActivity, PERMISSIONS_LOCATION,
+                        TAG_LOCATION_RESULTCODE);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    // Check Storage Permission
+    public boolean checkStoragePermission() {
+        if (ContextCompat.checkSelfPermission(mActivity,
+                Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(mActivity,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(mActivity,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE) || ActivityCompat
+                    .shouldShowRequestPermissionRationale(mActivity,
+                            Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                ActivityCompat.requestPermissions(mActivity, PERMISSIONS_STORAGE,
+                        TAG_STORAGE_READ_WRITE_RESULTCODE);
+            } else {
+                ActivityCompat.requestPermissions(mActivity, PERMISSIONS_STORAGE,
+                        TAG_STORAGE_READ_WRITE_RESULTCODE);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    // Check Camera Permission
+    public boolean checkCameraPermission() {
+        if (ContextCompat.checkSelfPermission(mActivity,
+                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(mActivity,
+                    Manifest.permission.CAMERA)) {
+                ActivityCompat.requestPermissions(mActivity, PERMISSIONS_CAMERA,
+                        TAG_CAMERA_RESULTCODE);
+            } else {
+                ActivityCompat.requestPermissions(mActivity, PERMISSIONS_CAMERA,
+                        TAG_CAMERA_RESULTCODE);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    // Check Record Audio Permission
+    public boolean checkRecordAudioPermission() {
+        if (ContextCompat.checkSelfPermission(mActivity,
+                Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(mActivity,
+                    Manifest.permission.RECORD_AUDIO)) {
+                ActivityCompat.requestPermissions(mActivity, PERMISSIONS_RECORD_AUDIO,
+                        TAG_RECOERD_AUDIO_RESULTCODE);
+            } else {
+                ActivityCompat.requestPermissions(mActivity, PERMISSIONS_RECORD_AUDIO,
+                        TAG_RECOERD_AUDIO_RESULTCODE);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    // Check SMS Permission
+    public boolean checkSmsPermission() {
+        if (ContextCompat.checkSelfPermission(mActivity,
+                Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(mActivity,
+                        Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(mActivity,
+                        Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(mActivity,
+                    Manifest.permission.READ_SMS) ||
+                    ActivityCompat.shouldShowRequestPermissionRationale(mActivity,
+                            Manifest.permission.SEND_SMS) ||
+                    ActivityCompat.shouldShowRequestPermissionRationale(mActivity,
+                            Manifest.permission.RECEIVE_SMS)) {
+                ActivityCompat.requestPermissions(mActivity, PERMISSIONS_SMS,
+                        TAG_SMS_RESULTCODE);
+            } else {
+                ActivityCompat.requestPermissions(mActivity, PERMISSIONS_SMS,
+                        TAG_SMS_RESULTCODE);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    // Check Contact Permission
+    public boolean checkContactsPermission() {
+        if (ContextCompat.checkSelfPermission(mActivity,
+                Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(mActivity,
+                        Manifest.permission.WRITE_CONTACTS) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(mActivity,
+                        Manifest.permission.GET_ACCOUNTS) != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(mActivity,
+                    Manifest.permission.READ_CONTACTS) ||
+                    ActivityCompat.shouldShowRequestPermissionRationale(mActivity,
+                            Manifest.permission.WRITE_CONTACTS) ||
+                    ActivityCompat.shouldShowRequestPermissionRationale(mActivity,
+                            Manifest.permission.GET_ACCOUNTS)) {
+                ActivityCompat.requestPermissions(mActivity, PERMISSIONS_CONTACTS,
+                        TAG_CONTACTS_RESULTCODE);
+            } else {
+                ActivityCompat.requestPermissions(mActivity, PERMISSIONS_CONTACTS,
+                        TAG_CONTACTS_RESULTCODE);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    // Check Sensor Permission
+    public boolean checkSensorPermission() {
+        if (ContextCompat.checkSelfPermission(mActivity,
+                Manifest.permission.BODY_SENSORS) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(mActivity,
+                    Manifest.permission.BODY_SENSORS)) {
+                ActivityCompat.requestPermissions(mActivity, PERMISSIONS_SENSORS,
+                        TAG_SENSORS_RESULTCODE);
+            } else {
+                ActivityCompat.requestPermissions(mActivity, PERMISSIONS_SENSORS,
+                        TAG_SENSORS_RESULTCODE);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    // Check Call Permission
+    public boolean checkCallPhonePermission() {
+        if (ContextCompat.checkSelfPermission(mActivity,
+                Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(mActivity,
+                    Manifest.permission.CALL_PHONE)) {
+                ActivityCompat.requestPermissions(mActivity, PERMISSIONS_CALL,
+                        TAG_CALLPHONE_RESULTCODE);
+            } else {
+                ActivityCompat.requestPermissions(mActivity, PERMISSIONS_CALL,
+                        TAG_CALLPHONE_RESULTCODE);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+}
