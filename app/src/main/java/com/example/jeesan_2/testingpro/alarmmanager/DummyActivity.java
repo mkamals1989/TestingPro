819e3c4f7e580695a20fbebd1d9cd11d8268e8e8
Kamal Selvaraj <mkamals1989@gmail.com>
2018-06-24 16:11:47 +0530

    Adds LogMe-Android Lib

--- app/src/main/java/com/example/jeesan_2/testingpro/alarmmanager/DummyActivity.java
+++ app/src/main/java/com/example/jeesan_2/testingpro/alarmmanager/DummyActivity.java
@@ -2,8 +2,16 @@ package com.example.jeesan_2.testingpro.alarmmanager;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 /**
  * Created by KAMAL on 2/1/2018.
@@ -37,5 +45,70 @@ public class DummyActivity extends AppCompatActivity {
                 });
         AlertDialog alert = builder.create();
         alert.show();
+    }
+
+    public class Client extends AsyncTask<Void, Void, Void> {
+
+        String dstAddress;
+        int dstPort;
+        String response = "";
+        TextView textResponse;
+
+        Client(String addr, int port, TextView textResponse) {
+            dstAddress = addr;
+            dstPort = port;
+            this.textResponse = textResponse;
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+
+            Socket socket = null;
+
+            try {
+                socket = new Socket(dstAddress, dstPort);
+
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(
+                        1024);
+                byte[] buffer = new byte[1024];
+
+                int bytesRead;
+                InputStream inputStream = socket.getInputStream();
+
+			/*
+             * notice: inputStream.read() will block if no data return
+			 */
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    byteArrayOutputStream.write(buffer, 0, bytesRead);
+                    response += byteArrayOutputStream.toString("UTF-8");
+                }
+
+            } catch (UnknownHostException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                response = "UnknownHostException: " + e.toString();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                response = "IOException: " + e.toString();
+            } finally {
+                if (socket != null) {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            textResponse.setText(response);
+            super.onPostExecute(result);
+        }
+
     }
 }
